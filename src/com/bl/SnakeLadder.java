54982285ac18ff5c80d6666206014993115c45af
sudha123 <sudhanaukala1997@gmail.com>
2022-11-09 16:41:47 +0530

    Added:rolls die

--- src/com/bl/SnakeLadder.java
+++ src/com/bl/SnakeLadder.java
@@ -4,7 +4,16 @@ public class SnakeLadder {
     static final int START_POSITION = 0;
 
     public static void main(String[] args) {
+        int dieNumber;
         int position = START_POSITION;
         System.out.println("current position : " + position);
+        dieNumber = rollDie();
+
+        System.out.println("Die Number : " + dieNumber);
+    }
+
+    static int rollDie() {
+        int random = (int) (Math.random() * 10 % 6);
+        return random;
     }
 }
